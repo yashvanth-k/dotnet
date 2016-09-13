780784a7c7be517347bf07a8cc268dc05915d346
Leandro Silveira Prado <leandro.prado@outlook.com.br>
2016-09-13 19:11:05 -0300

    Create the Util for manage HttpClient connection

---
+++ VSTSRestApiSamples/Util.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VstsRestApiSamples
+{
+    public class Util
+    {
+        public static HttpClient CreateConnection(IConfiguration _configuration, string _credentials)
+        {
+            if(String.IsNullOrEmpty(_configuration.UriString))
+                throw new Exception("Please enter the Uri String [appsetting.uri]");
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(_configuration.UriString);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", _credentials);
+            return client;
+        }
+    }
+}
