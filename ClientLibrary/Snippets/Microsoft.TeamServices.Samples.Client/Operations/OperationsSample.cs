205015e1f962e07116faf89127530dadb387a17d
Theophile Trunck <thtrunck@microsoft.com>
2017-08-17 16:42:46 -0400

    Add operation sample

---
+++ ClientLibrary/Snippets/Microsoft.TeamServices.Samples.Client/Operations/OperationsSample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.Services.Notifications.WebApi;
+using Microsoft.VisualStudio.Services.Operations;
+using Microsoft.VisualStudio.Services.WebApi;
+
+namespace Microsoft.TeamServices.Samples.Client.Operations
+{
+    /// <summary>
+    /// 
+    /// Sample showing how to use the Operation client to get the status of an operation.
+    /// 
+    /// For more details, see https://www.visualstudio.com/docs/integrate/api/operations/operations
+    /// 
+    /// </summary>
+    [ClientSample(OperationsResourceIds.AreaName, OperationsResourceIds.OperationsResource)]
+    public class OperationsSample : ClientSample
+    {
+        /// <summary>
+        /// Returns the operation for a known job (Team Foundation Server Cleanup).
+        /// </summary>
+        /// <returns></returns>
+        [ClientSampleMethod]
+        public Operation GetOperation()
+        {
+            VssConnection connection = Context.Connection;
+            OperationsHttpClient operationsClient = connection.GetClient<OperationsHttpClient>();
+
+            Operation operation = operationsClient.GetOperation(Guid.Parse("B19DDD28-9A95-42E2-9697-966FD822F1CD")).Result;
+
+            return operation;
+        }
+    }
+}
