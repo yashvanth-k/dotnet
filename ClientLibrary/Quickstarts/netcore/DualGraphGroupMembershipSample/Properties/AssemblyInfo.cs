81d1eb5d20d07b956d869da0729e59e56bdaaffb
Peaky Yuter <Peakyy@microsoft.com>
2017-09-07 17:39:55 -0700

    updating assembly.info

--- ClientLibrary/Quickstarts/netcore/DualGraphGroupMembershipSample/Properties/AssemblyInfo.cs
+++ ClientLibrary/Quickstarts/netcore/DualGraphGroupMembershipSample/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SimpleAdalConsoleApp")]
+[assembly: AssemblyTitle("DualGraphGroupMembershipSample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SimpleAdalConsoleApp")]
+[assembly: AssemblyProduct("DualGraphGroupMembershipSample")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
