d88b1d01a3a7f6041cb4768284bd1d4752b45973
Krishna Aditya <krishadi@microsoft.com>
2017-12-15 17:14:01 +0530

    samples for DG REST API - part 2

---
+++ ClientLibrary/Snippets/Microsoft.TeamServices.Samples.Client/DistributedTask/DeploymentGroupsMetricsSample.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.TeamFoundation.DistributedTask.WebApi;
+using Microsoft.VisualStudio.Services.WebApi;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.TeamServices.Samples.Client.DistributedTask
+{
+    [ClientSample(TaskResourceIds.AreaName, TaskResourceIds.DeploymentGroupsMetricsResource)]
+    class DeploymentGroupsMetricsSample : DeploymentGroupsSamplesBase
+    {
+        [ClientSampleMethod]
+        public IList<DeploymentGroupMetrics> ListAllDeploymentGroupMetrics()
+        {
+            // Get a connection and client instance
+            VssConnection connection = Context.Connection;
+            TaskAgentHttpClient dgClient = connection.GetClient<TaskAgentHttpClient>();
+
+            // Get all deployment groups' metrics
+            IList<DeploymentGroupMetrics> metrics = dgClient.GetDeploymentGroupsMetricsAsync2(this.ProjectName).SyncResult();
+
+            return metrics;
+        }
+
+        [ClientSampleMethod]
+        public DeploymentGroupMetrics GetDeploymentGroupMetricsByName()
+        {
+            // Get a connection and client instance
+            VssConnection connection = Context.Connection;
+            TaskAgentHttpClient dgClient = connection.GetClient<TaskAgentHttpClient>();
+
+            // Get deployment group's metrics by name            
+            DeploymentGroupMetrics metrics = dgClient.GetDeploymentGroupsMetricsAsync2(this.ProjectName, this.DemoDeploymentGroupName).SyncResult().FirstOrDefault();
+
+            return metrics;
+        }
+    }
+}
---
+++ ClientLibrary/Snippets/Microsoft.TeamServices.Samples.Client/DistributedTask/DeploymentTargetMessagesSample.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.TeamFoundation.DistributedTask.WebApi;
+using Microsoft.VisualStudio.Services.WebApi;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.TeamServices.Samples.Client.DistributedTask
+{
+    [ClientSample(TaskResourceIds.AreaName, TaskResourceIds.DeploymentTargetMessagesResource)]
+    class DeploymentTargetMessagesSample : DeploymentGroupsSamplesBase
+    {
+        [ClientSampleMethod]
+        public void UpgradeDeploymentTargetsInDeploymentGroup()
+        {
+            // Get a connection and client instance
+            VssConnection connection = Context.Connection;
+            TaskAgentHttpClient dgClient = connection.GetClient<TaskAgentHttpClient>();
+
+            // upgrade all deployment targets in demo deployment group
+            dgClient.RefreshDeploymentTargetsAsync(this.ProjectName, this.DemoDeploymentGroupId).SyncResult();
+        }
+    }
+}
