b74e8f8d8baf0de9485d81a05666366d6a015e8c
Octavio Licea León <ocleon@microsoft.com>
2019-06-20 11:43:26 -0400

    Make the audit samples async and support async ClientSampleMethodds

--- ClientLibrary/Samples/Audit/AuditLogSample.cs
+++ ClientLibrary/Samples/Audit/AuditLogSample.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.Services.Audit.WebApi;
 using Microsoft.VisualStudio.Services.WebApi;
 
@@ -9,19 +10,18 @@ namespace Microsoft.Azure.DevOps.ClientSamples.Audit
     public class AuditLogSample : ClientSample
     {
         [ClientSampleMethod]
-        public void QueryLogSample()
+        public async Task QueryLogSampleAsync()
         {
             try
             {
                 // Get the audit log client
                 VssConnection connection = Context.Connection;
-                AuditHttpClient auditClient = connection.GetClient<AuditHttpClient>();
+                AuditHttpClient auditClient = await connection.GetClientAsync<AuditHttpClient>();
 
                 // Query the audit log for the last day, take only 10 entries
                 DateTime now = DateTime.UtcNow;
-                AuditLogQueryResult logQueryResult = auditClient
-                    .QueryLogAsync(startTime: now.AddDays(-1), endTime: now, batchSize: 10, continuationToken: null)
-                    .SyncResult();
+                AuditLogQueryResult logQueryResult = await auditClient
+                    .QueryLogAsync(startTime: now.AddDays(-1), endTime: now, batchSize: 10, continuationToken: null);
 
                 Context.Log($"ContinuationToken:            {logQueryResult.ContinuationToken}");
                 Context.Log($"HasMore:                      {logQueryResult.HasMore}");
